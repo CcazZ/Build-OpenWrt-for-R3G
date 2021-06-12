a3590d70207d8b5f9d7c3358f47aae0b62dd33ca
CC <lcc.0705@qq.com>
2021-06-12 16:24:18 +0800

    fix error

--- diy.sh
+++ diy.sh
@@ -2,7 +2,7 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
 
-#集成DDNSto
+#集成DDNSto,项目地址：https://github.com/linkease/nas-packages
 echo >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 ./scripts/feeds update nas
