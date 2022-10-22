6cd2daf12128f951850d80b9e5b56c0180fe9330
CC <liangchangchang@yeah.net>
2022-10-22 22:06:46 +0800

    test

--- sh/test.sh
+++ sh/test.sh
@@ -1,47 +1,30 @@
 #!/bin/bash
-
-#初始化仓库函数
-init(){
-        sudo apt update -y
-        sudo apt full-upgrade -y
-        sudo apt install -y ack antlr3 aria2 asciidoc autoconf automake autopoint binutils bison build-essential 
-        sudo apt  install -y bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib 
-        sudo apt  install -y git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev 
-        sudo apt  install -y libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz 
-        sudo apt install -y  mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip libpython3-dev qemu-utils 
-        sudo apt install -y rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
-        sudo timedatectl set-timezone "Asia/Shanghai"
-}
-#下载第三方软件资源
-git(){
- echo $PWD;
-}
-#定义
-cloneResource(){
-		echo $PWD
-        #git clone --depth 1 https://github.com/siropboy/sirpdboy-package
-        #git clone --depth 1 https://github.com/kenzok8/openwrt-packages
-        #git clone --depth 1 https://github.com/liuran001/openwrt-packages
-        #git clone --depth 1 https://github.com/vernesong/OpenClash
-        git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb
-        git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr
-}
-
-feeds(){
-		./scripts/feeds clean
-        ./scripts/feeds update -a
-        ./scripts/feeds install -a
-}
-
 case $1 in
-        init)
-                init;;
+        
         clone)
-                cloneResource;;
+            echo $PWD
+			#git clone --depth 1 https://github.com/siropboy/sirpdboy-package
+			#git clone --depth 1 https://github.com/kenzok8/openwrt-packages
+			#git clone --depth 1 https://github.com/liuran001/openwrt-packages
+			#git clone --depth 1 https://github.com/vernesong/OpenClash
+			git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb
+			git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr
 		git)
-                git;;
+            git;;
 		feeds)
-				feeds;;
+			./scripts/feeds clean
+			./scripts/feeds update -a
+			./scripts/feeds install -a;;
+		init)
+            sudo apt update -y
+			sudo apt full-upgrade -y
+			sudo apt install -y ack antlr3 aria2 asciidoc autoconf automake autopoint binutils bison build-essential 
+			sudo apt  install -y bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib 
+			sudo apt  install -y git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev 
+			sudo apt  install -y libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz 
+			sudo apt install -y  mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip libpython3-dev qemu-utils 
+			sudo apt install -y rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+			sudo timedatectl set-timezone "Asia/Shanghai";;
         *)
         echo "非指定参数";;
 esac
