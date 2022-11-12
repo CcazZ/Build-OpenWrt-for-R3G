d9341b53775923665da7982d41b67723fbf66cd3
CC <lcc.0705@qq.com>
2022-11-13 00:21:31 +0800

    update

--- sh/r3g.sh
+++ sh/r3g.sh
@@ -6,7 +6,7 @@ case $1 in
 			#git clone --depth 1 https://github.com/siropboy/sirpdboy-package
 			#git clone --depth 1 https://github.com/kenzok8/openwrt-packages
 			#git clone --depth 1 https://github.com/liuran001/openwrt-packages
-			#git clone --depth 1 https://github.com/vernesong/OpenClash
+			git clone --depth 1 https://github.com/vernesong/OpenClash
 			git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb
 			git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr;;
 		addSrc)
