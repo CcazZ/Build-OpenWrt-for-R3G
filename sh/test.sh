aca4a9cbdf9348c5764e7caa186da3c07dbd48c5
CC <lcc.0705@qq.com>
2022-11-24 19:40:11 +0800

    test

--- sh/test.sh
+++ sh/test.sh
@@ -12,8 +12,8 @@ case $1 in
 			;;
 		addSrc)
 		    echo $PWD
-            #echo src-git helloworld https://github.com/fw876/helloworld >> feeds.conf.default
-			echo src-git passwall https://github.com/xiaorouji/openwrt-passwall >> feeds.conf.default
+            echo src-git helloworld https://github.com/fw876/helloworld >> feeds.conf.default
+			#echo src-git passwall https://github.com/xiaorouji/openwrt-passwall >> feeds.conf.default
 			;;
 		feeds)
 			./scripts/feeds clean
